a4186a86d291a17ec922a175abe1fe05358b224d
Vasyl Bodnar <vasyl_bodnar@outlook.com>
2026-03-24 18:01:38 -0400

    Adjusted format to use 4 bytes

--- asset/hex-to-bin.scm
+++ asset/hex-to-bin.scm
@@ -13,9 +13,9 @@
 ;; Twice the real one
 (define (row-type->count row-type)
   (case row-type
-    ((wide) 64)
+    ((wide)   64)
     ((normal) 32)
-    ((short) 16)))
+    ((short)  16)))
 
 (define (count->row-type count)
   (case count
@@ -25,16 +25,21 @@
 
 (define (row-type->hinibble row-type)
   (case row-type
-    ((wide) #x100000)
-    ((normal) #x000000)
-    ((short) #x200000)))
+    ((wide)   #x10000000)
+    ((normal) #x00000000)
+    ((short)  #x20000000)))
+
+(define (put-two-num-bv bv tmp-bv ein zwei)
+  (bytevector-u32-set! tmp-bv 0 ein (endianness little))
+  (bytevector-u32-set! tmp-bv 4 zwei (endianness little))
+  (put-bytevector bv tmp-bv 0 8))
 
 ;; Simple format
 ;; HEXFONT0
-;; (repeat (3 byte range-start, where highest nibble is row-type, inclusive)
-;;         (3 byte range-end, exclusive))
-;; (000030 ends the ranges with invalid 30 byte)
-;; (000000 as padding)
+;; (repeat (4 byte range-start, where highest nibble is row-type, inclusive)
+;;         (4 byte range-end, exclusive))
+;; (00000030 ends the ranges with invalid 30 byte)
+;; (00000000 as padding)
 ;; (repeat bitmaps of a 8, 16, or 32 byte value depending on row-type per range
 ;;         the number of bitmaps is then simply (range-end - range-start))
 (call-with-input-file (string-append filename ".hex")
@@ -51,9 +56,9 @@
                         (row-type 'normal)
                         (range-start 0) (range-end 0 code-point))
                        ((eof-object? line)
-                        (bytevector-uint-set! number-bv 0 (logior (row-type->hinibble row-type) range-start) (endianness little) 3)
-                        (bytevector-uint-set! number-bv 3 range-end (endianness little) 3)
-                        (put-bytevector bin number-bv 0 6))
+                        (put-two-num-bv bin number-bv
+                                        (logior (row-type->hinibble row-type) range-start)
+                                        range-end))
                      (let* ((halves (string-split line #\:))
                             (new-code-point (string->number (car halves) 16))
                             (new-row-type (count->row-type (string-length (cadr halves))))
@@ -62,13 +67,13 @@
                        (put-bytevector content-port number-bv 0 (/ (row-type->count new-row-type) 2))
                        (unless (and (= new-code-point code-point)
                                     (eq? new-row-type row-type))
-                         (bytevector-uint-set! number-bv 0 (logior (row-type->hinibble row-type) range-start) (endianness little) 3)
-                         (bytevector-uint-set! number-bv 3 range-end (endianness little) 3)
-                         (put-bytevector bin number-bv 0 6)
+                         ;; Header is built here
+                         (put-two-num-bv bin number-bv
+                                         (logior (row-type->hinibble row-type) range-start)
+                                         range-end)
                          (set! range-start new-code-point)
                          (set! row-type new-row-type))
                        (set! code-point (1+ new-code-point))))))))
-          ;; The header ends with (little-endian) value #x000030, which is invalid for any range-start value
-          (bytevector-uint-set! number-bv 0 #x300000 (endianness little) 3)
-          (put-bytevector bin number-bv 0 3)
+          ;; The header ends with (little-endian) value #x00000030, which is invalid for any range-start value
+          (put-two-num-bv bin number-bv #x30000000 #x00000000)
           (put-bytevector bin content-bv))))))
